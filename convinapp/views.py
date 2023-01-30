1d10923cc5fecd3ec571da56c97fff02861c8de1
Prakhar Pratyush <prakhar841301@gmail.com>
2023-01-31 03:04:37 +0530

    error-handling: Add status codes and error handling.
    This commit adds meaningful status codes and messages to views.
    
    Implements Error-handling for helper functions.

--- convinapp/views.py
+++ convinapp/views.py
@@ -2,6 +2,7 @@ from helpers.fetch_events import fetch_events
 from helpers.fetch_token import fetch_token
 from helpers.initiate_authorization import initiate_authorization
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.views import APIView
 import webbrowser
 
@@ -14,13 +15,20 @@ data = {
 class GoogleCalendarInitView(APIView):
     
     def get(self, request):
-        # fetch authorization url
-        authorization_url, state = initiate_authorization(
-            data['client_secret'],
-            data['scopes'],
-            data['redirect_uri'],
-            access_type='offline',
-            include_granted_scopes='true')
+
+        try:
+            # fetch authorization url
+            authorization_url, state = initiate_authorization(
+                data['client_secret'],
+                data['scopes'],
+                data['redirect_uri'],
+                access_type='offline',
+                include_granted_scopes='true')
+        except:
+            return Response(
+                data="Unexpected Error, check server logs.",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
         try:
             # for cross-client compatibilty, we use 'webbrowwer.open()'
@@ -29,9 +37,11 @@ class GoogleCalendarInitView(APIView):
         except:
             # If client was unable to open browser, suggest the user
             # to manually open it.
-            return Response(data="Open this url in your browser {}".format(authorization_url))
+            return Response(
+                data="Open this url in your browser {}".format(authorization_url),
+                status=status.HTTP_200_OK)
         # code will never reach here.
-        return Response(status=200)
+        return Response(status=status.HTTP_200_OK)
 
 class GoogleCalendarRedirectView(APIView):
     
@@ -39,15 +49,24 @@ class GoogleCalendarRedirectView(APIView):
         code = request.query_params['code']
 
         # Exchange 'access_token' and 'refresh_token' for
-        # auth_code. 
-        credentials = fetch_token(
-            data['client_secret'],
-            data['scopes'],
-            data['redirect_uri'],
-            code=code)
+        # auth_code.
+        try:
+            credentials = fetch_token(
+                data['client_secret'],
+                data['scopes'],
+                data['redirect_uri'],
+                code=code)
+        except:
+            return Response(
+                data="Unexpected Error, check server logs.",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         # Make a call to GoogleCalendarAPI to fetch events
         # list with their start-times.
-        events = fetch_events(credentials)
-        # events = json.dumps(events)
-        return Response(data=events)
+        try:
+            events = fetch_events(credentials)
+        except:
+            return Response(
+                data="Failed to retrieve events list, check server logs.",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(data=events, status=status.HTTP_200_OK)
--- helpers/fetch_events.py
+++ helpers/fetch_events.py
@@ -1,24 +1,18 @@
 from googleapiclient.discovery import build
 
 def fetch_events(credentials):
-    try:
-        service = build('calendar', 'v3', credentials=credentials)
+    service = build('calendar', 'v3', credentials=credentials)
 
-        # Call the Calendar API
-        events_result = service.events().list(calendarId='primary').execute()
-        events = events_result.get('items', [])
+    # Call the Calendar API
+    events_result = service.events().list(calendarId='primary').execute()
+    events = events_result.get('items', [])
 
-        if not events:
-            return []
+    if not events:
+        return []
 
-        # Prepare events_dict
-        events_dict = {}
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            events_dict[start] = event['summary']
-
-    except:
-        # we will return meaningful error message from
-        # the APIView.
-        return -1
+    # Prepare events_dict
+    events_dict = {}
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        events_dict[start] = event['summary']
     return events_dict
