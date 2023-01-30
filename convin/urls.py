7cf2905e09e1e6f4270ca7ff6d739ad336fba420
Prakhar Pratyush <prakhar841301@gmail.com>
2023-01-30 17:02:57 +0530

    urls: Configure url patterns for the project.
    This commit adds the views (no functionality) and
    configures the url patterns.
    
    Settings is updated to include rest_framework and
    convinapp.

--- convin/urls.py
+++ convin/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from convinapp.views import GoogleCalendarInitView, GoogleCalendarRedirectView
+
+v1_api_urlpatterns = [
+    path('v1/calendar/init/', GoogleCalendarInitView.as_view()),
+    path('v1/calendar/redirect/', GoogleCalendarRedirectView.as_view()),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest/', include(v1_api_urlpatterns)),
 ]
